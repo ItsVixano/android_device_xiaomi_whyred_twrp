2bd3e50d7ab75732d238c3749a0c51efe5ac3884
ItsVixano <vixanonew@gmail.com>
2021-02-13 11:25:36 +0100

    whyred: Cleanup unused stuffs
    Change-Id: I9c367aaf77d59c96a9122c3121887bbbed466dcf

--- omni_whyred.mk
+++ omni_whyred.mk
@@ -16,26 +16,14 @@
 # limitations under the License.
 #
 
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, build/target/product/embedded.mk)
-
-# Inherit Telephony packages
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit language packages
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Inherit 64bit support
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := whyred
 PRODUCT_NAME := omni_whyred
 PRODUCT_BRAND := xiaomi
-PRODUCT_MODEL := Xiaomi Redmi Note 5 Pro
+PRODUCT_MODEL := Xiaomi Redmi Note 5
 PRODUCT_MANUFACTURER := xiaomi
-PRODUCT_RELEASE_NAME := Xiaomi Redmi Note 5 Pro
+PRODUCT_RELEASE_NAME := Xiaomi Redmi Note 5
